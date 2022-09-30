a2e49e07cc9afa04e5844924e6caf8caec960aa0
Jon <jonathancerruti@gmail.com>
2022-09-29 22:54:37 -0700

    thoughtsController written

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const { connect, connection } = require('mongoose');
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/social_networkDB';
 
-connect(connectionString, {
+connect(connectionString, { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
